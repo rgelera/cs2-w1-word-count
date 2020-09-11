cf11e7fff5bed9335e0c6dbf42f8cb5751c4fe60
Rodney Gelera <rodney.gelera@shopify.com>
2020-09-10 20:02:03 -0700

    start

---
+++ wordcount.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+  char input;
+  int word_count = 0;
+  int longest_word = 0;
+  int largest_num_vowels = 0;
+
+  while(input != '\n') {
+    input = cin.get();
+  }
+}
